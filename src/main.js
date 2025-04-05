0d46524384bef00361f9356e6ca774dbd64d0aa7
Alan <alantan321353@gmail.com>
2025-04-05 14:16:59 +0100

    trying locar

--- src/main.js
+++ src/main.js
@@ -1,24 +1,85 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
+import * as LocAR from 'locar';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.001, 1000);
 
-setupCounter(document.querySelector('#counter'))
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const locar = new LocAR.LocationBased(scene, camera);
+
+window.addEventListener("resize", e => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+});
+
+const cam = new LocAR.WebcamRenderer(renderer);
+
+let firstLocation = true;
+
+let deviceOrientationControls = new LocAR.DeviceOrientationControls(camera);
+
+locar.on("gpsupdate", (pos, distMoved) => {
+  if (firstLocation) {
+    alert(`Got the initial location: longitude ${pos.coords.longitude}, latitude ${pos.coords.latitude}`);
+
+    const boxProps = [{
+      latDis: 0.0005,
+      lonDis: 0,
+      colour: 0xff0000
+    }, {
+      latDis: -0.0005,
+      lonDis: 0,
+      colour: 0xffff00
+    }, {
+      latDis: 0,
+      lonDis: -0.0005,
+      colour: 0x00ffff
+    }, {
+      latDis: 0,
+      lonDis: 0.0005,
+      colour: 0x00ff00
+    }];
+
+    const geom = new THREE.BoxGeometry(10, 10, 10);
+
+    for (const boxProp of boxProps) {
+      const mesh = new THREE.Mesh(
+        geom,
+        new THREE.MeshBasicMaterial({ color: boxProp.colour })
+      );
+
+      console.log(`adding at ${pos.coords.longitude + boxProp.lonDis},${pos.coords.latitude + boxProp.latDis}`);
+      locar.add(
+        mesh,
+        pos.coords.longitude + boxProp.lonDis,
+        pos.coords.latitude + boxProp.latDis
+      );
+    }
+
+    firstLocation = false;
+  }
+});
+
+locar.startGps();
+
+document.getElementById("setFakeLoc").addEventListener("click", e => {
+  alert("Using fake input GPS, not real GPS location");
+  locar.stopGps();
+  locar.fakeGps(
+    parseFloat(document.getElementById("fakeLon").value),
+    parseFloat(document.getElementById("fakeLat").value)
+  );
+});
+
+renderer.setAnimationLoop(animate);
+
+function animate() {
+  cam.update();
+  deviceOrientationControls?.update();
+  renderer.render(scene, camera);
+}
